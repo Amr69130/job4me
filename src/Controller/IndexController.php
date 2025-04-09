0e150530349f7094c336ed1d0aa4f2cba1172c97
Amr69130 <amrouchenorris@gmail.com>
2025-04-09 16:14:22 +0200

    Add Tailwind CSS configuration and create job offers and partners templates
    - Added Tailwind CSS configuration file for styling.
    - Created job offers template with a responsive grid layout to display job listings.
    - Developed partners template to showcase partner companies and their job offers.
    - Implemented navigation bar for easy access to job offers and partners pages.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\JobOfferRepository;
+use App\Repository\CompanyRepository;
+
 
 final class IndexController extends AbstractController
 {
@@ -15,4 +18,28 @@ final class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
+
+    #[Route('/joboffers', name: 'app_joboffers')]
+    public function jobOffers(JobOfferRepository $jobOfferRepository): Response
+    {
+        // Récupérer toutes les offres d'emploi depuis la base de données
+        $jobOffers = $jobOfferRepository->findAll();
+
+        // Passer les offres d'emploi à la vue
+        return $this->render('index/joboffers.html.twig', [
+            'jobOffers' => $jobOffers,
+        ]);
+    }
+
+    #[Route('/partners', name: 'app_partners')]
+    public function partners(CompanyRepository $companyRepository): Response
+    {
+        $companies = $companyRepository->findAll();
+
+        return $this->render('index/partners.html.twig', [
+            'companies' => $companies,
+        ]);
+    }
+
+
 }
